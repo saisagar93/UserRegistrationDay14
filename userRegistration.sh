52beb814fee7c791cad7504656ffb2bf298a3fcd
saisagar93 <sscreator225@gmail.com>
2020-12-19 10:43:30 +0530

    code to check email validation with the all possiblities and merge uc3 into master

--- userRegistration.sh
+++ userRegistration.sh
@@ -18,3 +18,13 @@ then
 else
         echo Invalid
 fi
+
+read -p "Enter email id : " email
+emailPattern="^([[:lower:]]|[0-9])([._+\-]?([0-9]|[[:lower:]]))*[@]([[:lower:]]|[0-9])*[.][[:lower:]]{2,6}([.][[:lower:]]{2,6})?$"
+
+if [[ $email =~ $emailPattern ]]
+then
+	echo Valid
+else
+	echo Invalid
+fi
