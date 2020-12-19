a02ca8aac0c2373cc9c123df9c4e4754cc399acd
saisagar93 <sscreator225@gmail.com>
2020-12-19 10:22:39 +0530

    code to check first name validation first latter must be capital

--- userRegistration.sh
+++ userRegistration.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
 echo "Wellcome to User Registration"
+read -p "Enter First Name : " firstName
+firstNamePattern="^[[:upper:]]{1}[[:lower:]]{2,}$"
+if [[ $firstName =~ $firstNamePattern ]]
+then
+        echo Valid
+else
+        echo Invalid
+fi
 
