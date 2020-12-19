b920875cd3b4c04c81962368bc4681e8d7a53e02
saisagar93 <sscreator225@gmail.com>
2020-12-19 11:34:53 +0530

    Add

--- userRegistration.sh
+++ userRegistration.sh
@@ -54,3 +54,11 @@ then
 else
         echo Invalid
 fi
+read -p "Enter password : " password
+passwordPattern="^(.*[[:upper:]].*).{8}([0-9]{1})$"
+if [[ $password =~ $passwordPattern ]]
+then
+        echo Valid
+else
+        echo Invalid
+fi
